1bab0dd2f7deba8c317b6c087a8caa0a189e0541
YoshinoriN <nayo-msoft@outlook.jp>
2017-01-31 23:48:08 +0900

    数値の表示

--- Kuru-C/Kuru-C.c
+++ Kuru-C/Kuru-C.c
@@ -15,6 +15,11 @@ int main()
 	printf("マクリスティ！\n");
 	printf("マクリスティ！\tマクリスティ！\n");
 	printf("マクリスティ！\tマクリスティ！\n");
+
+	printf("%d", 1900);
+	printf("\n%d年", 1900);
+
+	printf("\n%d+%d=%d\n", 100, 200, 300);
     return 0;
 }
 
