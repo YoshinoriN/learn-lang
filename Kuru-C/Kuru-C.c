44a92507770ebdc5baaad7ec5a0f6b46c52ecb26
YoshinoriN <nayo-msoft@outlook.jp>
2017-01-31 23:11:40 +0900

    改行と頭ぞろえ

--- Kuru-C/Kuru-C.c
+++ Kuru-C/Kuru-C.c
@@ -7,6 +7,14 @@
 int main()
 {
 	printf("hello world");
+	printf("\n");
+	printf("改行\n");
+	printf("し\n");
+	printf("マクリスティ！\n\n");
+	printf("マクリスティ！\n");
+	printf("マクリスティ！\n");
+	printf("マクリスティ！\tマクリスティ！\n");
+	printf("マクリスティ！\tマクリスティ！\n");
     return 0;
 }
 
