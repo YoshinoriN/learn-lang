ce781b6b76a5d5fc8d5432410e64423cda8da321
YoshinoriN <nayo-msoft@outlook.jp>
2017-02-02 17:56:55 +0900

    数値計算

--- Kuru-C/Kuru-C.c
+++ Kuru-C/Kuru-C.c
@@ -20,11 +20,26 @@ int main()
 	printf("マクリスティ！\tマクリスティ！\n");
 	printf("マクリスティ！\tマクリスティ！\n");
 
-	//数値
+	//整数(%dを使う)
 	printf("%d", 1900);
 	printf("\n%d年", 1900);
 
 	printf("\n%d+%d=%d\n", 100, 200, 300, "\n\n");
+
+	//整数計算
+	printf("\n%d", 10 + 3);
+	printf("\n%d", 10 - 3);
+	printf("\n%d", 10 * 3);
+	printf("\n%d", 10 / 3);
+
+	printf("\n");
+
+	//実数計算(%fを使う)
+	printf("\n%f", 10.0 + 3.0);
+	printf("\n%f", 10.0 - 3.0);
+	printf("\n%f", 10.0 * 3.0);
+	printf("\n%f", 10.0 / 3.0);
+
     return 0;
 }
 
