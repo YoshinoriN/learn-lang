7e61176a6b59e25c10686bb2ee745ca49caa2527
YoshinoriN <nayo-msoft@outlook.jp>
2017-02-02 17:46:38 +0900

    コメント追記

--- Kuru-C/Kuru-C.c
+++ Kuru-C/Kuru-C.c
@@ -7,19 +7,24 @@
 int main()
 {
 	printf("hello world");
+
+	//改行
 	printf("\n");
 	printf("改行\n");
 	printf("し\n");
 	printf("マクリスティ！\n\n");
 	printf("マクリスティ！\n");
 	printf("マクリスティ！\n");
+	
+	//インデントそろえる
 	printf("マクリスティ！\tマクリスティ！\n");
 	printf("マクリスティ！\tマクリスティ！\n");
 
+	//数値
 	printf("%d", 1900);
 	printf("\n%d年", 1900);
 
-	printf("\n%d+%d=%d\n", 100, 200, 300);
+	printf("\n%d+%d=%d\n", 100, 200, 300, "\n\n");
     return 0;
 }
 
