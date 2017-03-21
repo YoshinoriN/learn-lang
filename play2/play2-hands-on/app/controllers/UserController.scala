84580be5ab7975f1f8148b1893a58ae9b721a0a0
YoshinoriN <nayo-msoft@outlook.jp>
2017-03-22 01:10:26 +0900

    Fix typo

--- play2/play2-hands-on/app/controllers/UserController.scala
+++ play2/play2-hands-on/app/controllers/UserController.scala
@@ -3,7 +3,7 @@ package controllers
 import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
-import play.api.libs.concurrent.Execution.Implicits.defaultContent
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.i18n.{MessagesApi, I18nSupport}
 import play.api.db.slick._
 import slick.driver.JdbcProfile
@@ -17,8 +17,8 @@ import slick.driver.H2Driver.api._
   * また、インスタンスの取得だけでなく、トレイトをミックスインする（ざっくり書くと機能の一部を使用する）
   */
 class UserController @Inject()(val dbConfigProvider: DatabaseConfigProvider,
-                               val messageApi: MessagesApi) extends Controller
-    with HasDatabaseConfigProvider[JdbcProfile] with I18nSupport {
+                               val messagesApi: MessagesApi) extends Controller
+  with HasDatabaseConfigProvider[JdbcProfile] with I18nSupport {
 
   /**
     * 一覧表示
