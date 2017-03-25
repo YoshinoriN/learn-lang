de1fd87c934b962ef0ab806575994d958f0b06e5
YoshinoriN <nayo-msoft@outlook.jp>
2017-03-25 16:21:54 +0900

    Add Convert Scala object to JSON object

--- play2/play2-hands-on/app/controllers/JsonController.scala
+++ play2/play2-hands-on/app/controllers/JsonController.scala
@@ -13,6 +13,28 @@ import slick.driver.H2Driver.api._
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
 
+//play2のJSONサポートによるDSLでの記述
+object JsonController {
+  // Scalaオブジェクト(UsersRow)をJSONに変換するにはWritesを定義
+  implicit val usersRowWritesWrites = (
+    (__ \ "id"       ).write[Long]   and
+    (__ \ "name"     ).write[String] and
+    (__ \ "companyId").writeNullable[Int]
+    )(unlift(UsersRow.unapply))
+}
+
+/* play2のJSONサポートによるDSLを使わない場合
+implicit val usersRowWritesFormat = new Writes[UsersRow]{
+  def writes(user: UsersRow): JsValue = {
+    Json.obj(
+      "id"        -> user.id,
+      "name"      -> user.name,
+      "companyId" -> user.companyId
+    )
+  }
+}
+*/
+
 class JsonController @Inject()(val dbConfigProvider: DatabaseConfigProvider) extends Controller
   with HasDatabaseConfigProvider[JdbcProfile] {
 
