96609005eefb271b37d54aad312e1bad5c7f53fc
YoshinoriN <nayo-msoft@outlook.jp>
2018-09-17 22:40:11 +0900

    Create stations data

---
+++ utils/station/stationDistanceType.ml
@@ -0,0 +1,21 @@
+type stationDistanceType = {
+  start : string;
+  finish : string;
+  transit : string;
+  distance : float;
+  time : int;
+}
+
+(*
+  #use "stationDistanceType.ml";;
+
+  Result
+  -------------------------
+  type stationDistanceType = {
+    start : string;
+    finish : string;
+    transit : string;
+    distance : float;
+    time : int;
+  }
+*)
---
+++ utils/station/stationType.ml
@@ -0,0 +1,12 @@
+type stationType = {
+  name : string;
+  line : string;
+}
+
+(*
+  #use "stationType.ml";;
+
+  Result
+  -------------------------
+  type stationType = { name : string; line : string; }
+*)
