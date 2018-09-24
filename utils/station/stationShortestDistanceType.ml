e9bb5fd2db55bfe028c48d6b738c321b7f6e72f4
YoshinoriN <nayo-msoft@outlook.jp>
2018-09-24 18:59:19 +0900

    Chapter 12 ex1

---
+++ utils/station/stationShortestDistanceType.ml
@@ -0,0 +1,17 @@
+type stationShortestDistanceType = {
+  name : string;
+  shortestDistance : float;
+  beforStationList: string list;
+}
+
+(*
+  #use "stationShortestDistanceType.ml";;
+
+  Result
+  -------------------------
+  type stationShortestDistanceType = {
+    name : string;
+    shortestDistance : float;
+    beforStationList : string list;
+  }
+*)
