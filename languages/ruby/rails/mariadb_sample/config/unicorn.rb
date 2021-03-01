9fa4ce65f66ca5139a41dbaad672c4188c2bd7c0
yoshinorin <yoshinorin.net@outlook.com>
2021-03-02 08:46:20 +0900

    update

--- languages/ruby/rails/mariadb_sample/config/unicorn.rb
+++
@@ -1,5 +0,0 @@
-worker_processes 1
-listen  "3000" 
-pid     '/tmp/unicorn.pid'
-stderr_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
-stdout_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
